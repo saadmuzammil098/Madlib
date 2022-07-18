31cd0e55e3a93567522c3e0957e3d39b87880e54
Saad Muzammil <46246456+saadmuzammil098@users.noreply.github.com>
2022-07-18 13:43:49 +0500

    Madlib.py
    Create simple Madlibs using format string

---
+++ Madlib.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 13:47:39 2022
+
+@author: SaadMuzammil
+"""
+
+adj = input("Adjective: ")
+verb_1 = input("Verb: ")
+verb_2 = input("verb: ")
+famous_person = input("Famous Person: ")
+
+
+madlib = f"Computer programming is so {adj} It makes me so excited all the time because\
+ I love to {verb_1}. Stay hydrated and {verb_2} like you are {famous_person}!"
+
+print(madlib)
